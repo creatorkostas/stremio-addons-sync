4afaa97399cb1f173f56494b6b62768a4a464b24
Konstantinos Mokas <kostasmokas12@gmail.com>
2025-07-04 19:37:30 +0300

    Add umami analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,8 @@ import { ThemeProvider } from "next-themes"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Next.js Community Starter",
-  description: "A modern Next.js starter with theme support",
-    generator: 'v0.dev'
+  title: "Stremio Addons Sync",
+  description: "Sync your Stremio Addons with your exported data",
 }
 
 export default function RootLayout({
@@ -19,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+    <head>
+        <script defer src="https://umami.kostasmokas.com/script.js" data-website-id="af1f2276-fd27-43c9-bd11-bea67d0115d3"></script>
+    </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
